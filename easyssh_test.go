78c55c6227a73c7250aa5b8d7f7acc2d88bf556d
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-03-04 16:13:00 +0800

    update coverage
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- easyssh_test.go
+++ easyssh_test.go
@@ -190,8 +190,25 @@ func TestProxyClient(t *testing.T) {
 		},
 	}
 
-	// password of target client is incorrect.
-	// can't connect target server
+	// proxy client can't dial to target server
+	session, err = ssh.connect()
+	assert.Nil(t, session)
+	assert.Error(t, err)
+
+	ssh = &MakeConfig{
+		Server:   "localhost",
+		User:     "drone-scp",
+		Port:     "22",
+		Password: "123456",
+		Proxy: defaultConfig{
+			User:    "drone-scp",
+			Server:  "localhost",
+			Port:    "22",
+			KeyPath: "./tests/.ssh/id_rsa",
+		},
+	}
+
+	// proxy client can't create new client connection of target
 	session, err = ssh.connect()
 	assert.Nil(t, session)
 	assert.Error(t, err)
