178d3384c204056976e015b79bc05ad87ede41ed
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-05-08 14:36:36 +0800

    fix: potential nil err

--- easyssh.go
+++ easyssh.go
@@ -266,17 +266,20 @@ func (ssh_conf *MakeConfig) Scp(sourceFile string, etargetFile string) error {
 	}
 
 	go func() {
-		w, _ := session.StdinPipe()
+		w, stdErr := session.StdinPipe()
+
+		if stdErr != nil {
+			return stdErr
+		}
+		defer w.Close()
 
 		fmt.Fprintln(w, "C0644", srcStat.Size(), targetFile)
 
 		if srcStat.Size() > 0 {
 			io.Copy(w, src)
 			fmt.Fprint(w, "\x00")
-			w.Close()
 		} else {
 			fmt.Fprint(w, "\x00")
-			w.Close()
 		}
 	}()
 
